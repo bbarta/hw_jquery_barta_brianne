4b69eddb1671c76931d45cfbe44b2526739c4d3d
Brianne Barta <brianne28@ufl.edu>
2017-03-31 21:45:31 -0400

    Implement paragraph styling

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ $( document ).ready(function() {
     var headings = $('h2');
     $( 'article' ).last().text( 'I\'m Brianne Barta, and I\'m learning jQuery!');
 });
+
+
+$( document ).ready(function() {
+    var paragraphs = $('p');
+    $( 'p' ).css({"color": "blue", "background-color": #DCDCDC, "fontWeight": "bold"});
+});
