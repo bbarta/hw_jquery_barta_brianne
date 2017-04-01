852ee18835c2aab5af0ec0e383300d516a2c8e80
Brianne Barta <brianne28@ufl.edu>
2017-03-31 21:37:45 -0400

    Finalize changes to last in article Reconnect jQuery with revised script tag

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $( document ).ready(function() {
     var headings = $('h2');
-    $( 'article' ).last().text( "I'm Brianne Barta, and I'm learning jQuery!" );
+    $( 'article' ).last().text( 'I\'m Brianne Barta, and I\'m learning jQuery!');
 });
